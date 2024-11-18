d5ef2f69a06b10b205ebecc3bc3dc4c6c7af370d
Alec Malenfant <113561614+AlecMalenfant001@users.noreply.github.com>
2024-11-18 00:43:39 -0600

    More buttons (#16)
    * add delete description button
    
    * word changes
    
    * add random word buttons
    
    * fix component reference

--- components/reference-image-footer.jsx
+++ components/reference-image-footer.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ConfidenceCircle from "./confidence-circle";
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
+import CancelIcon from "@mui/icons-material/Cancel";
 import Grid from "@mui/material/Grid2";
 
 export default function ReferenceImageFooter(props) {
@@ -10,21 +11,29 @@ export default function ReferenceImageFooter(props) {
     <>
       <Box sx={{ marginTop: 2 }}>
         {/*Headers*/}
-        <Grid container spacing={1} columns={10}>
+        <Grid container spacing={1} columns={12}>
           <Grid size={{ xs: 5, sm: 7 }}>
             <Typography variant="h6">Description</Typography>
           </Grid>
           <Grid size={{ xs: 5, sm: 3 }}>
             <Typography variant="h6">Confidence</Typography>
           </Grid>
+          <Grid size={{ xs: 2, sm: 2 }}>
+            <Typography variant="h6">Delete</Typography>
+          </Grid>
         </Grid>
         {/*Content*/}
-        <Grid container spacing={1} columns={10}>
+        <Grid container spacing={1} columns={12}>
           <Grid size={{ xs: 5, sm: 7 }}>
             <Typography variant="body1">{props.description}</Typography>
           </Grid>
           <Grid size={{ xs: 5, sm: 3 }}>
             <ConfidenceCircle value={props.confidence} />
+          </Grid>
+          <Grid size={{ xs: 2, sm: 2 }}>
+            <IconButton onClick={props.removeFunction}>
+              <CancelIcon />
+            </IconButton>
           </Grid>
         </Grid>
       </Box>
